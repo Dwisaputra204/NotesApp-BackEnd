ce654f3c04ab9f1cb451abd6eecdab9af8e7eb50
Dwisaputra204 <dwisaputra204@gmail.com>
2021-07-09 07:27:00 +0700

    auth

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved */
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 require('dotenv').config();
 
